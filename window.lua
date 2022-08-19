db75a9034b0f95652a5c7d7bbb1b191c94bcd0f0
tycho <jh.leong@outlook.com>
2022-08-19 18:08:18 +0800

    chore(window): support focus main display

--- window.lua
+++ window.lua
@@ -26,19 +26,16 @@ hs.hotkey.bind(hyperKey, "d", function()
     WinWin:moveToScreen('right')
 end)
 
--------------- 聚焦 & 移动光标 至下一个屏幕 -----------------
+-------------- focus display -----------------
 
-local function focus_other_screen(isNext) -- focuses the other screen 
-    local screen = hs.mouse.getCurrentScreen()
-
-    local nextScreen = isNext and screen:next() or screen:previous()
-
-    local rect = nextScreen:fullFrame()
+function moveMouse2TargetDisplay(display)
+    local rect = display:fullFrame()
     local center = hs.geometry.rectMidPoint(rect)
     hs.mouse.absolutePosition(center)
 end
 
-function get_window_under_mouse() -- from https://gist.github.com/kizzx2/e542fa74b80b7563045a 
+-- * code from: https://gist.github.com/kizzx2/e542fa74b80b7563045a 
+function getDisplayUnderMouse()
     local my_pos = hs.geometry.new(hs.mouse.absolutePosition())
     local my_screen = hs.mouse.getCurrentScreen()
     return hs.fnutils.find(hs.window.orderedWindows(), function(w)
@@ -46,53 +43,19 @@ function get_window_under_mouse() -- from https://gist.github.com/kizzx2/e542fa7
     end)
 end
 
-function activate_other_screen(isNext)
-    focus_other_screen(isNext)
-    local win = get_window_under_mouse()
-    -- now activate that window 
-    win:focus()
+function activateDisplay(display)
+    moveMouse2TargetDisplay(display)
+    getDisplayUnderMouse():focus()
 end
 
-hs.hotkey.bind(hyperKey, "i", function()
-    activate_other_screen(true)
+-- focus main display
+hs.hotkey.bind(hyperKey, "p", function()
+    activateDisplay(hs.screen.primaryScreen())
 end)
 
+-- focus next display
 hs.hotkey.bind(hyperKey, "o", function()
-    activate_other_screen(false)
+    activateDisplay(hs.mouse.getCurrentScreen():next())
 end)
 
--------------- 将窗口移动至其他 space -----------------
-
--- function moveWindowOneSpace(direction)
---     local mouseOrigin = hs.mouse.absolutePosition()
---     local win = hs.window.focusedWindow()
---     local clickPoint = win:zoomButtonRect()
-
---     clickPoint.x = clickPoint.x + clickPoint.w + 5
---     clickPoint.y = clickPoint.y + (clickPoint.h / 2)
-
---     local mouseClickEvent = hs.eventtap.event.newMouseEvent(hs.eventtap.event.types.leftmousedown, clickPoint)
---     mouseClickEvent:post()
---     hs.timer.usleep(150000)
-
---     local nextSpaceDownEvent = hs.eventtap.event.newKeyEvent({"ctrl"}, direction, true)
---     nextSpaceDownEvent:post()
---     hs.timer.usleep(150000)
-
---     local nextSpaceUpEvent = hs.eventtap.event.newKeyEvent({"ctrl"}, direction, false)
---     nextSpaceUpEvent:post()
---     hs.timer.usleep(150000)
-
---     local mouseReleaseEvent = hs.eventtap.event.newMouseEvent(hs.eventtap.event.types.leftmouseup, clickPoint)
---     mouseReleaseEvent:post()
---     hs.timer.usleep(150000)
-
---     hs.mouse.setAbsolutePosition(mouseOrigin)
--- end
-
--- hs.hotkey.bind(hyperKey, "e", function()
---     moveWindowOneSpace("right")
--- end)
--- hs.hotkey.bind(hyperKey, "q", function()
---     moveWindowOneSpace("left")
--- end)
+-------------- focus display end -----------------
